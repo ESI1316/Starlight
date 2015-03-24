7050f74d39fee5d4950498457213fc35218c6e43
Kriwin Paul <39171@heb.be>
2015-03-24 13:56:24 +0100

    -ajout test levelfactory

---
+++ test/levelfactorytest.cpp
@@ -0,0 +1,33 @@
+#include "catch.hpp"
+#include "model/level.h"
+#include "model/levelFactory.h"
+
+TEST_CASE("creation of a level from a file", "levelFactory::getLevel(std::string)")
+{
+    Level * level = levelFactory::getLevelFromFile("./test/mapFileSample.map");
+
+    REQUIRE(level != 0);
+
+    SECTION("checking sizes of vectors")
+    {
+        REQUIRE(level->getWalls().size() == 9);
+        REQUIRE(level->getMirrors().size() == 11);
+        REQUIRE(level->getCrystals().size() == 1);
+        REQUIRE(level->getNukes().size() == 2);
+        REQUIRE(level->getLenses().size() == 1);
+        REQUIRE(level->getRays().size() == 0);
+    }
+
+    SECTION("checking correctness of components attributes value")
+    {
+        REQUIRE(level->getSource().getPosition() == Point(0, 0));
+        REQUIRE(level->getSource().getEdge() == 29);
+        REQUIRE(level->getSource().getAngle() == 4.75);
+        REQUIRE(level->getSource().getWavelength() == 400);
+
+        REQUIRE(level->getDestination().getPosition() == Point(721, 275))
+        REQUIRE(level->getDestination().getEdge() == 29);
+    }
+
+    delete level;
+}
