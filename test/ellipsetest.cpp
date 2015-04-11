9bc13d937fcb8b1f709bdcfdd6913153c457272d
Placentino Simon <39631@heb.be>
2015-04-11 14:13:03 +0200

    Issue #1 @Ellipse#getIntersectionsPoints(const Line &);

--- test/ellipsetest.cpp
+++ test/ellipsetest.cpp
@@ -1,6 +1,7 @@
 #include "test/catch.hpp"
 
 #include "model/geometry/ellipse.hpp"
+#include "model/geometry/line.hpp"
 #include "model/starlightexception.hpp"
 
 TEST_CASE("Constructeur d'ellipse")
@@ -10,13 +11,13 @@ TEST_CASE("Constructeur d'ellipse")
     REQUIRE_THROWS_AS(Ellipse(0.96, -2.8091279786, Point(5.22, 3.)), StarlightException);
     REQUIRE_THROWS_AS(Ellipse(0., -2.8091279786, Point(5.22, 3.)), StarlightException);
 }
-/*
+
 TEST_CASE("Accesseurs d'ellipse")
 {
-    Ellipse ellipse{0.96, 2.8091279786, Point{5.22, 3.}};
-    REQUIRE(ellipse.getCenter() == Point(5.22, 3.));
-    REQUIRE(utilities::equals(ellipse.getXRadius(), 0.96));
-    REQUIRE(utilities::equals(ellipse.getYRadius(), 2.8091279786));
+    Ellipse ellipse{2.8091279786 / 2., 1.92 / 2., Point{2., 2.}};
+    REQUIRE(ellipse.getCenter() == Point(2., 2.));
+    REQUIRE(utilities::equals(ellipse.getXRadius(), pow(2.8091279786 / 2., 2)));
+    REQUIRE(utilities::equals(ellipse.getYRadius(), pow(1.92 / 2., 2)));
 }
 
 TEST_CASE("Operateurs d'ellipse")
@@ -34,7 +35,11 @@ TEST_CASE("Operateurs d'ellipse")
 
 TEST_CASE("Get points d'intersection")
 {
-    Ellipse ellipse{7.8912, 0.9216, Point{2., 2.}};
-}
+    Ellipse ellipse{2.8091279786 / 2., 1.92 / 2., Point{2., 2.}};
+    Line line{0.2682926829, 0.9697560976};
+    std::vector<Point> intersections = ellipse.getIntersectionPoints(line);
 
-*/
+    REQUIRE_FALSE(intersections.empty());
+    REQUIRE(intersections.at(0) == Point(0.68087158, 1.1524289605));
+    REQUIRE(intersections.at(1) == Point(4.7223754929, 2.2367348883));
+}
