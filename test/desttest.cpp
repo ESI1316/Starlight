42bfe2d1613b3bd88ceeb60f006515826fa591b5
Placentino Simon <39631@heb.be>
2015-04-13 13:29:33 +0200

    dest testes added

--- test/desttest.cpp
+++ test/desttest.cpp
@@ -1,3 +1,35 @@
 #include "test/catch.hpp"
 #include "model/dest.hpp"
 #include "model/geometry/rectangle.hpp"
+#include "model/starlightexception.hpp"
+
+TEST_CASE("Dest : constructeurs")
+{
+    REQUIRE_THROWS_AS(Dest(Point(2, 2), 0), StarlightException);
+    REQUIRE_THROWS_AS(Dest(Point(2, 2), -1), StarlightException);
+    REQUIRE_NOTHROW(Dest(Point(2, 2), 3));
+}
+
+TEST_CASE("Dest : accesseurs")
+{
+    Dest dest{Point{2., 3.}, 18};
+
+    REQUIRE(dest.getEdge() == 18);
+    REQUIRE_FALSE(dest.isLightedUp());
+    dest.setLightedUp(true);
+    REQUIRE(dest.isLightedUp());
+}
+
+TEST_CASE("Dest : operateurs")
+{
+    Dest dest{Point{2., 3.}, 18};
+    Dest de{Point{2., 3.}, 18};
+    Dest d{Point{2., 3.}, 48};
+
+    REQUIRE(dest == de);
+    REQUIRE_FALSE(dest != de);
+    REQUIRE(dest != d);
+
+    de.setLightedUp(true);
+    REQUIRE_FALSE(dest == de);
+}
