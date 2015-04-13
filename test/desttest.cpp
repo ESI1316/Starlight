ee8c414d7fb040a490724d2d703eba9e97f01700
Placentino Simon <39631@heb.be>
2015-04-13 13:49:45 +0200

    End of dest testes

--- test/desttest.cpp
+++ test/desttest.cpp
@@ -1,6 +1,9 @@
 #include "test/catch.hpp"
 #include "model/dest.hpp"
 #include "model/geometry/rectangle.hpp"
+#include "model/geometry/utilities.hpp"
+#include "model/point.hpp"
+#include "model/ray.hpp"
 #include "model/starlightexception.hpp"
 
 TEST_CASE("Dest : constructeurs")
@@ -32,4 +35,60 @@ TEST_CASE("Dest : operateurs")
 
     de.setLightedUp(true);
     REQUIRE_FALSE(dest == de);
+}
+
+TEST_CASE("Dest : includeRay")
+{
+    Dest dest{Point{5.4, 3.}, 18};
+
+    SECTION("droite quelconque : pas d'intersection")
+    {
+        Ray ray{Point{2.7766139723, -2.5169499277}, 0.2415937631, 649};
+
+        Point * intersection = dest.includeRay(ray);
+        REQUIRE(intersection == 0);
+    }
+    SECTION("droite quelconque : intersection")
+    {
+        Ray ray{Point{2.7766139723, -2.5169499277}, 0.4413298865, 649};
+
+        Point * intersection = dest.includeRay(ray);
+        REQUIRE(intersection != 0);
+        REQUIRE(*intersection == Point(23.4, 7.2256651053));
+        delete intersection;
+    }
+
+    SECTION("droite verticale : pas d'intersection")
+    {
+        Ray ray{Point{2.7766139723, -2.5169499277}, utilities::PI_2, 649};
+
+        Point * intersection = dest.includeRay(ray);
+        REQUIRE(intersection == 0);
+    }
+
+    SECTION("droite verticale : intersection")
+    {
+        Ray ray{Point{9.7766139723, -2.5169499277}, utilities::PI_2, 649};
+
+        Point * intersection = dest.includeRay(ray);
+        REQUIRE(intersection != 0);
+        REQUIRE(*intersection == Point(9.7766139723, 21));
+        delete intersection;
+    }
+    SECTION("droite verticale : pas d'intersection")
+    {
+        Ray ray{Point{-5.132736737, 25.752491025}, 0., 649};
+
+        Point * intersection = dest.includeRay(ray);
+        REQUIRE(intersection == 0);
+    }
+    SECTION("droite verticale : intersection")
+    {
+        Ray ray{Point{-5.132736737, 12.752491025}, 0., 649};
+
+        Point * intersection = dest.includeRay(ray);
+        REQUIRE(intersection != 0);
+        REQUIRE(*intersection == Point(23.4, 12.752491025));
+        delete intersection;
+    }
 }
