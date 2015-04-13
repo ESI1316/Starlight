6aba3a33d859d6952d7926a356a2160b4e7090b1
Placentino Simon <39631@heb.be>
2015-04-13 11:21:12 +0200

    Nuke test indications

--- test/nuketest.cpp
+++ test/nuketest.cpp
@@ -1,6 +1,7 @@
 #include "test/catch.hpp"
 #include "model/nuke.hpp"
 #include "model/starlightexception.hpp"
+#include "model/geometry/utilities.hpp"
 
 TEST_CASE("construction")
 {
@@ -12,17 +13,20 @@ TEST_CASE("accesseurs et mutateurs")
 {
     Nuke duke(Nuke(Point(4., 3.), 10.));
 
-    REQUIRE(duke.getRadius() == 10.);
+    REQUIRE(utilities::equals(duke.getRadius(), 10.));
     REQUIRE(duke.getLocation() == Point(4., 3.));
-    REQUIRE(duke.isLightedUp() == false);
+    REQUIRE_FALSE(duke.isLightedUp());
     duke.setLightedUp(true);
-    REQUIRE(duke.isLightedUp() == true);
+    REQUIRE(duke.isLightedUp());
 }
 
 TEST_CASE("ReactToRay(Ray)")
 {
     Nuke duke(Nuke(Point(4., 3.), 10.));
     // je sais pas comment créer un ray :(
+    // Ray ray(point de depart, angle de tir, wavelenght)
+    // tu créer une droite sur geogebra, tu utilises l'outils pour trouver l'angle (en radian)
+    // de l'intersection avec l'axe des x
 }
 
 TEST_CASE("includeRay(const Ray &)")
