e3a987aa0486b40842f89934e31be4ade8e1f966
Kriwin Paul <39171@heb.be>
2015-04-21 13:16:27 +0200

    -Movement mirrorview semi implemented

--- view/dynamicElements/mirrorview.cpp
+++ view/dynamicElements/mirrorview.cpp
@@ -7,7 +7,9 @@
 
 MirrorView::MirrorView(Mirror * mirror)
     : QGraphicsLineItem(QLineF{viewUtilities::toQPoint(mirror->getStart()),
-                        viewUtilities::toQPoint(mirror->getEnd())}), mirror{mirror}
+                        viewUtilities::toQPoint(mirror->getEnd())}),
+      mirror{mirror}, boundRect{}
+
 {
     QPen pen{QColor{204, 204, 255}};
 
@@ -15,7 +17,26 @@ MirrorView::MirrorView(Mirror * mirror)
 
     this->setPen(pen);
     QGraphicsLineItem::setCursor(QCursor(Qt::PointingHandCursor));
+    this->setFlag(QGraphicsItem::ItemIsMovable);
 }
 
 MirrorView::~MirrorView() {}
+
+QVariant MirrorView::itemChange(QGraphicsItem::GraphicsItemChange change, const QVariant &value)
+{
+    if (change == ItemPositionChange && this->scene())
+    {
+            QPointF newPos = value.toPointF();
+            QRectF rect{};
+
+            if (!rect.contains(newPos))
+            {
+                newPos.setX(qMin(rect.right(), qMax(newPos.x(), rect.left())));
+                newPos.setY(qMin(rect.bottom(), qMax(newPos.y(), rect.top())));
+
+                return newPos;
+            }
+     }
+        return QGraphicsLineItem::itemChange(change, value);
+}
 
--- view/dynamicElements/mirrorview.hpp
+++ view/dynamicElements/mirrorview.hpp
@@ -2,17 +2,24 @@
 #define MIRRORVIEW_HPP
 
 #include <QGraphicsLineItem>
+#include <QRectF>
 
 #include "model/elements/mirror.hpp"
 
 class MirrorView : public QGraphicsLineItem
 {
     Mirror * mirror;
+    QRectF boundRect;
 
 public:
 
     MirrorView(Mirror * mirror);
     ~MirrorView();
+
+protected:
+
+    QVariant itemChange(GraphicsItemChange, const QVariant &);
+
 };
 
 #endif // MIRRORVIEW_HPP
